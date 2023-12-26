b3f51e74cbbb7ef4489983b2a54287e792ef2ca8
MD. Razikul Islam Joni <razikuljoni@gmail.com>
2023-12-26 22:04:36 +0600

    add searchbar toggle and show searchbar in the bottom of the navbar and and style search input field

---
+++ script.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body"),
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light"),
+    searchToggle = document.querySelector(".searchToggle"),
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    siderbarClose = document.querySelector(".siderbarClose");
+
+// js code to toggle search box
+searchToggle.addEventListener("click", () => {
+    searchToggle.classList.toggle("active");
+});
