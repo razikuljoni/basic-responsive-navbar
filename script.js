a4c191ee53405b60d48bf161ebc3f4f7ef474242
MD. Razikul Islam Joni <razikuljoni@gmail.com>
2023-12-26 23:29:16 +0600

    make responsive for small devices, toggle sidebar menu for small devices, open and close options

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ modeToggle.addEventListener("click", () => {
 searchToggle.addEventListener("click", () => {
     searchToggle.classList.toggle("active");
 });
+
+// toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+// close sidebar click anywhere in body
+body.addEventListener("click", e => {
+    let clickedElm = e.target;
+    if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+        nav.classList.remove("active");
+    }
+});
