ffa273619c41eb0a17fdd27856f9a9b9278fe865
MD. Razikul Islam Joni <razikuljoni@gmail.com>
2023-12-26 22:27:28 +0600

    change light and dark mode clicking the icon and set user prefference in localstorage

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ const body = document.querySelector("body"),
     sidebarOpen = document.querySelector(".sidebarOpen"),
     siderbarClose = document.querySelector(".siderbarClose");
 
+// get user selected mode from local storage
+let getMode = localStorage.getItem("mode");
+
+// checking user selected mode and adding class
+if (getMode && getMode === "dark-mode") {
+    body.classList.add("dark");
+}
+
+// toggle dark and light mode
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark");
+    // keep user selected mode even page refresh or file reopen
+    if (!body.classList.contains("dark")) {
+        localStorage.setItem("mode", "light-mode");
+    } else {
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
+
 // js code to toggle search box
 searchToggle.addEventListener("click", () => {
     searchToggle.classList.toggle("active");
